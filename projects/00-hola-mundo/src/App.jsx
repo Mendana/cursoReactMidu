0980f64655deb756a04995003303f2845bcaacaf
Mendana <diegodiazmenda@gmail.com>
2025-02-19 23:27:48 +0100

    Video 1 - Min 1:26:40

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -8,8 +8,7 @@ export function App(){
         <section class="App">
             <TwitterFollowCard userName="midudev" name="Miguel Ángel Durán" isFollowing/>
             <TwitterFollowCard userName="pheralb" name="Pablo Hernández" isFollowing/>
-            <TwitterFollowCard userName="elonmusk" name="Elon Musk" isFollowing={false}/>
-            <TwitterFollowCard isFollowing userName="vxnder" name="Vanderhart"/>
+            <TwitterFollowCard userName="vxnder" name="Vanderhart" isFollowing={false}/>
         </section>
     )
 }
