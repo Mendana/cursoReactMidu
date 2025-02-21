514a7e055f371dfa53c1a755b534a2c83e9e8d14
Mendana <diegodiazmenda@gmail.com>
2025-02-21 23:40:27 +0100

    Video 1 - Min 2:01:44

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -2,13 +2,19 @@ import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
 
 export function App(){
-    const formatUserName = (userName) => `@${userName}`
-
     return(
         <section class="App">
-            <TwitterFollowCard userName="midudev" name="Miguel Ángel Durán" isFollowing/>
-            <TwitterFollowCard userName="pheralb" name="Pablo Hernández" isFollowing/>
-            <TwitterFollowCard userName="vxnder" name="Vanderhart" isFollowing={false}/>
+            <TwitterFollowCard userName="midudev" isFollowing>
+                Miguel Ángel Durán	
+            </TwitterFollowCard>
+
+            <TwitterFollowCard userName="pheralb" isFollowing>
+                Pablo Hernández
+            </TwitterFollowCard>
+            
+            <TwitterFollowCard>
+                Juanpe
+            </TwitterFollowCard>
         </section>
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,20 +1,37 @@
-export function TwitterFollowCard({userName, name, isFollowing}) {
-    const imgSrc = `https://unavatar.io/${userName}`
-    return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img src={imgSrc} alt="El ávatar de midudev" className='tw-followCard-avatar'/>
-                <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>@{userName}</span>
-                </div>
-            </header>
+import { useState } from "react";
 
-            <aside>
-                <button className='tw-followCard-button'>
-                    {isFollowing ? 'Siguiendo' : 'Seguir'}
-                </button>
-            </aside>
-        </article>
-    )
+export function TwitterFollowCard({children,  userName = "unknown"}) {
+
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const imgSrc = `https://unavatar.io/${userName}`;
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing ?
+    'tw-followCard-button is-following' : 'tw-followCard-button';
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  }
+
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          src={imgSrc}
+          alt={`El ávatar de ${userName}`}
+          className="tw-followCard-avatar"
+        />
+        <div className="tw-followCard-info">
+          <strong>{children}</strong>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className={buttonClassName} onClick={handleClick}>
+            {text}
+        </button>
+      </aside>
+    </article>
+  );
 }
