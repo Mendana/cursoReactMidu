56119da91d36d8e84add6329151d7d85b4a971bc
Mendana <diegodiazmenda@gmail.com>
2025-02-22 23:27:56 +0100

    Video 2 - Min 00:26:40

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,10 +1,29 @@
-import './App.css';
-import { TwitterFollowCard } from './TwitterFollowCard';
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
 
-export function App(){
-    return(
-        <section class="App">
-            <TwitterFollowCard userName="midudev" isFollowing>
+const users = [
+  {
+    userName: "midudev",
+    name: "Miguel Ángel Durán",
+    isFollowing: true,
+  },
+  {
+    userName: "pheralb",
+    name: "Pablo Hernández",
+    isFollowing: true,
+  },
+  {
+    userName: null,
+    name: "Juanpe",
+    isFollowing: false,
+  },
+];
+
+export function App() {
+  return (
+    <section className="App">
+      {/* Como se harían uno a uno */}
+      {/* <TwitterFollowCard userName="midudev" isFollowing>
                 Miguel Ángel Durán	
             </TwitterFollowCard>
 
@@ -14,7 +33,16 @@ export function App(){
             
             <TwitterFollowCard>
                 Juanpe
-            </TwitterFollowCard>
-        </section>
-    )
+            </TwitterFollowCard> */}
+      {/* Como hacer todos del tirón */}
+      {users.map((user) => {
+        const { userName, name, isFollowing } = user;
+        return (
+          <TwitterFollowCard userName={userName} isFollowing={isFollowing} key = {userName}>
+            {name}
+          </TwitterFollowCard>
+        );
+      })}
+    </section>
+  );
 }
